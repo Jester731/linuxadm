b7e76be2b266deb647332a1fbc0ab98b05bef3b6
Jester731 <zavyalov.mikhail99@gmail.com>
2021-03-17 19:51:33 +0500

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import os
+import sys
+
+forks = int(sys.argv[1])
+iterations = int(sys.argv[2])
+message = sys.argv[3]
+    
+child_pids = []
+bash = os.getppid()
+
+for i in range(1, forks + 1):
+    pid = os.fork()
+    if (pid == 0):
+        for j in range(iterations * i):
+            if(os.getppid() == bash): exit(-1)
+            print(f'fork_id = {i}', f'iter_id = {j}', f'msg = {message}', sep='\t')
+        exit(0)
+    else:
+        child_pids.append(pid)
+        
+for pid in child_pids:
+    os.waitpid(pid, 0)
